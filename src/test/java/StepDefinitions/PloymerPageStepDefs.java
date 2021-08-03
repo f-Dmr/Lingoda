0c2699502a9f1af251f148b959e5c91cacbb954e
f-Dmr <fdemirwrk@gmail.com>
2021-08-03 18:26:41 +0300

    Waits added to catch what is happening

--- src/test/java/StepDefinitions/PloymerPageStepDefs.java
+++ src/test/java/StepDefinitions/PloymerPageStepDefs.java
@@ -12,35 +12,37 @@ import java.util.concurrent.TimeUnit;
 
 public class PloymerPageStepDefs {
     PolymerPage polymerPage = new PolymerPage();
+    Actions actions = new Actions(Driver.getdriver());
 
     @Then("User should add two todo items")
     public void user_should_add_two_todo_items() {
 
+        BrowserUtils.waitFor(2);
         polymerPage.firstItem.sendKeys(ConfigurationReader.get("toDo1"));
-        Actions actions = new Actions(Driver.getdriver());
+
         actions.sendKeys(Keys.ENTER).perform();
 
-        //        BrowserUtils.waitFor(3);
+        BrowserUtils.waitFor(2);
 
         polymerPage.firstItem.sendKeys(ConfigurationReader.get("toDo2"));
         actions.sendKeys(Keys.ENTER).perform();
 
-        // BrowserUtils.waitFor(3);
+        BrowserUtils.waitFor(2);
 
 
     }
 
     @Then("User should be able to edit second item")
     public void user_should_be_able_to_edit_second_item() {
-        Actions actions = new Actions(Driver.getdriver());
+
         actions.doubleClick(polymerPage.secondItem).perform();
-        // BrowserUtils.waitFor(3);
+        BrowserUtils.waitFor(2);
         actions.doubleClick(polymerPage.editItem).perform();
-        // BrowserUtils.waitFor(3);
+        BrowserUtils.waitFor(2);
         polymerPage.editItem.sendKeys(Keys.BACK_SPACE);
-        // BrowserUtils.waitFor(3);
+        BrowserUtils.waitFor(2);
         polymerPage.editItem.sendKeys(ConfigurationReader.get("edittedItem"),Keys.ENTER);
-        //   BrowserUtils.waitFor(3);
+        BrowserUtils.waitFor(2);
 
     }
 }
